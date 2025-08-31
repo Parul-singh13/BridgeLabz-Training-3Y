83d6378d4818fcebd89a71ab6658a5ef8aff1b4e
Parul-singh13 <145196891+Parul-singh13@users.noreply.github.com>
2025-08-31 15:51:10 +0530

    Add files via upload
    Array Assignment Level1

---
+++ CheckAge.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class CheckAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] age = new int[10];
+
+        
+        for (int i = 0; i < age.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            age[i] = sc.nextInt();
+        }
+
+        // Voting eligibility check
+        for (int i = 0; i < age.length; i++) {
+            if (age[i] <= 0) {
+                System.out.println("Invalid age entered for student " + (i + 1));
+            } else if (age[i] >= 18) {
+                System.out.println("The student with age " + age[i] + " can vote.");
+            } else {
+                System.out.println("The student with age " + age[i] + " cannot vote.");
+            }
+        }
+        
+        sc.close();
+    }
+}
