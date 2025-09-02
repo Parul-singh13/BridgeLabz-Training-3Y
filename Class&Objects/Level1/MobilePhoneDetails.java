329deb990d9fba4b3575c5a8cc6880f63989c9a4
Parul-singh13 <145196891+Parul-singh13@users.noreply.github.com>
2025-09-02 17:13:45 +0530

    Add files via upload

---
+++ Class&Objects/Level1/MobilePhoneDetails.java
@@ -0,0 +1,25 @@
+public class MobilePhoneDetails {
+    private String brand;
+    private String model;
+    private double price;
+
+    // Constructor
+    public MobilePhoneDetails(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    // Method to display details
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: $" + price);
+    }
+
+    // Main method to demonstrate
+    public static void main(String[] args) {
+        MobilePhoneDetails phone = new MobilePhoneDetails("Samsung", "Galaxy S21", 799.99);
+        phone.displayDetails();
+    }
+}
